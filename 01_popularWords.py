a18c2ae2fba2f2a26cf77994d4a1ea5182375b63
TheBoBa2 <glooplootloo@gmail.com>
2026-07-02 23:44:00 +0300

    added task 01

---
+++ 01_popularWords.py
@@ -0,0 +1,20 @@
+﻿def popular_words(text, words):
+    text = text.lower().split()
+    result = {}
+
+    for n in words:
+        result[n] = text.count(n)
+    return result
+
+
+assert popular_words(
+    '''When I was One I had just begun When I was Two I was nearly new''',
+    ['i', 'was', 'three', 'near']
+) == {
+    'i': 4,
+    'was': 3,
+    'three': 0,
+    'near': 0
+}, 'Test1'
+
+print("OK")
